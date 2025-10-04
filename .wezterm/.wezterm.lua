6c688c78995a1914583921c8df0f3695e143cdaf
NguyenXuanVinh <nongami87@gmail.com>
2025-10-04 11:40:31 +0700

    feat: update appearance and font settings
    - switches to a new color scheme and customizes cursor styling for better visibility.
    - enhances font appearance by using bold weight, increasing size, and adjusting line height.
    - modifies tab bar and window padding for improved UI layout and usability.

--- .wezterm/.wezterm.lua
+++ .wezterm/.wezterm.lua
@@ -2,28 +2,35 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
--- font settings
-config.font = wezterm.font("DankMono Nerd Font")
-config.font_size = 12.0
-
 -- color scheme
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Poimandres"
 
--- default: launch Arch WSL
-config.default_prog = { "wsl.exe", "-d", "Arch" }
+-- cursor
+config.default_cursor_style = "BlinkingBlock"
+config.colors = {
+  cursor_bg = "#A6ACCD",
+  cursor_border = "#A6ACCD",
+  cursor_fg = "#1B1E28",
+}
 
--- disable tab bar
-config.enable_tab_bar = false
+-- font
+config.font = wezterm.font("DankMono Nerd Font", { weight = "Bold" });
+config.font_size = 12.5
+config.line_height = 1.8
 
--- keep border, remove titlebar
+-- tab bar
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_max_width = 999999
+
+-- window
+config.window_padding = {
+  left = 30, right = 30, top = 30, bottom = 30,
+}
 config.window_decorations = "RESIZE"
 
--- remove padding for full usage
-config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
-}
+-- default program
+config.default_prog = { "wsl.exe", "-d", "Arch" }
 
 return config
