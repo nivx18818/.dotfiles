8228bc5ce3d4e06698a3d7b09ce85ff27469a5d5
NguyenXuanVinh <nongami87@gmail.com>
2025-10-08 09:40:10 +0700

    fix(nvim/auto-save): enable condition to skip gitcommit buffers
    - prevents auto-saving files in buffers used for git commit messages, avoiding unwanted saves and potential workflow disruption.
    - ensures auto-save only runs on modifiable, non-gitcommit buffers.

--- nvim/lua/plugins/auto-save.lua
+++ nvim/lua/plugins/auto-save.lua
@@ -6,7 +6,7 @@ return {
   opts = {
     debounceDelay = 1000,
   },
-  -- condition = function(buf)
-  --   return vim.bo[buf].filetype ~= "gitcommit" and vim.bo[buf].modifiable
-  -- end,
+  condition = function(buf)
+    return vim.bo[buf].filetype ~= "gitcommit" and vim.bo[buf].modifiable
+  end,
 }
