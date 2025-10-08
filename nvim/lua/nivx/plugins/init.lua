dc922a0875fbb84162d3f327035e60843e25650d
NguyenXuanVinh <nongami87@gmail.com>
2025-10-08 10:40:11 +0700

    refactor(nvim/plugins): restructure plugin organization
    - groups plugins into categorized subdirectories for better maintainability and clarity.
    - introduces a central module to manage plugin imports, streamlining plugin loading and organization.
    - no logic changes to plugin configurations.

---
+++ nvim/lua/nivx/plugins/init.lua
@@ -0,0 +1,9 @@
+return {
+  spec = {
+    { import = "plugins.core" },
+    { import = "plugins.ui" },
+    { import = "plugins.git" },
+    { import = "plugins.coding" },
+    { import = "plugins.tools" },
+  },
+}
