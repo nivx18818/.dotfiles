44befa613f08c535849c81dee6775fed7c676be0
NguyenXuanVinh <nongami87@gmail.com>
2025-10-08 19:07:19 +0700

    feat(nvim/keymaps): add yank to last non-blank char mapping
    - introduces a new key mapping to yank text from the first to the last non-blank character in a line, improving text manipulation efficiency and aligning with user expectations for intuitive yanking operations.

--- nvim/lua/nivx/core/keymaps.lua
+++ nvim/lua/nivx/core/keymaps.lua
@@ -1,5 +1,8 @@
 local map = vim.keymap.set
 
+-- Yank from first to last non-blank character
+map("n", "Y", "^yg_", { desc = "Yank from first to last non-blank character", silent = true })
+
 -- Better up/down
 map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
 map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
