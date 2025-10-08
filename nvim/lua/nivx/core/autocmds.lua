aa4a822a71fade0c1fafa30dc0f1013313942362
NguyenXuanVinh <nongami87@gmail.com>
2025-10-08 19:01:14 +0700

    feat(nvim/autocmds): add q to close select filetypes
    - introduces an autocmd for several common filetypes to allow closing buffers with 'q', improving navigation and buffer management for transient or informational windows.
    - also refactors highlight-yank autocmd for improved API compatibility.

--- nvim/lua/nivx/core/autocmds.lua
+++ nvim/lua/nivx/core/autocmds.lua
@@ -1,8 +1,46 @@
+local function augroup(name)
+  return vim.api.nvim_create_augroup(name, { clear = true })
+end
+
 -- Highlight when yanking (copying) text
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup("highlight_yank"),
   callback = function()
-    vim.hl.on_yank()
+    (vim.hl or vim.highlight).on_yank()
+  end,
+})
+
+-- Close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("close_with_q"),
+  pattern = {
+    "PlenaryTestPopup",
+    "checkhealth",
+    "dbout",
+    "gitsigns-blame",
+    "grug-far",
+    "help",
+    "lspinfo",
+    "neotest-output",
+    "neotest-output-panel",
+    "neotest-summary",
+    "notify",
+    "qf",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.schedule(function()
+      vim.keymap.set("n", "q", function()
+        vim.cmd("close")
+        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+      end, {
+        buffer = event.buf,
+        silent = true,
+        desc = "Quit buffer",
+      })
+    end)
   end,
 })
