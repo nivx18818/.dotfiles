d74d27d2d6a4b0eef233c87f95503748673e1f57
NguyenXuanVinh <nongami87@gmail.com>
2025-10-08 17:58:56 +0700

    feat(nvim/lsp): enhance formatter config and keymap
    - refactors formatter configuration to support multiple formatters per filetype, adds new default format options, and updates key mappings to cover normal and visual modes.
    - improves flexibility and reliability of autoformatting integration.

--- nvim/lua/nivx/plugins/lsp/conform.lua
+++ nvim/lua/nivx/plugins/lsp/conform.lua
@@ -1,19 +1,34 @@
 return { -- Autoformat
   "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
+  dependencies = { "mason.nvim" },
+  lazy = true,
+  cmd = "ConformInfo",
   keys = {
     {
       "<leader>cf",
       function()
-        require("conform").format({ async = true, lsp_format = "fallback" })
+        require("conform").format({{ async = true, lsp_format = "fallback" }})
       end,
-      mode = "",
+      mode = { "n", "v" },
       desc = "Format",
     },
   },
   opts = {
-    notify_on_error = false,
+    default_format_opts = {
+      timeout_ms = 3000,
+      async = false,
+      quite = false,
+      lsp_format = "fallback",
+      try_node_executable = true,
+    },
+    formatters_by_ft = {
+      lua = { "stylua" },
+      javascript = { "prettier", "prettierd", stop_after_first = true },
+    },
+    formatters = {
+      injected = { options = { ignore_errors = true } },
+    },
+    -- notify_on_error = false,
     format_on_save = false,
     -- format_on_save = function(bufnr)
     --   local disable_filetypes = {}
@@ -26,13 +41,6 @@ return { -- Autoformat
     --     }
     --   end
     -- end,
-    formatters_by_ft = {
-      lua = { "stylua" },
-      javascript = { "prettierd", "prettier", stop_after_first = true },
-    },
-    default_format_opts = {
-      try_node_executable = true,
-    },
     mason_install = true,
   },
 }
