7020a5458f061fed4bccde17c4eb58ac5dcc07b4
NguyenXuanVinh <nongami87@gmail.com>
2025-10-08 17:57:27 +0700

    feat(nvim/ui): enhance Neo-tree startup and UX actions
    - improves file explorer by autostarting on directory open, adding commands to copy file paths and open with system apps, and refining keybindings and integration with the terminal.
    - enhances overall UX with better lifecycle management and configuration options.

--- nvim/lua/nivx/plugins/ui/neo-tree.lua
+++ nvim/lua/nivx/plugins/ui/neo-tree.lua
@@ -1,25 +1,45 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   version = "v3.x",
+  cmd = "Neotree",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim"
+    "MunifTanjim/nui.nvim",
   },
   lazy = false,
   keys = {
-    { "<leader>e",  "<cmd>Neotree reveal<cr>", desc = "Reveal Explorer" },
-    { "<leader>fe", "<cmd>Neotree focus<cr>",  desc = "Focus Explorer" },
+    { "<leader>e", "<cmd>Neotree reveal<cr>", desc = "Reveal Explorer" },
   },
+  deactivate = function()
+    vim.cmd([[Neotree close]])
+  end,
+  init = function()
+    vim.api.nvim_create_autocmd("BufEnter", {
+      group = vim.api.nvim_create_augroup("Neotree_start_directory", { clear = true }),
+      desc = "Start Neo-tree with directory",
+      once = true,
+      callback = function()
+        if package.loaded["neo-tree"] then
+          return
+        else
+          local stats = vim.uv.fs_stat(vim.fn.argv(0))
+          if stats and stats.type == "directory" then
+            require("neo-tree")
+          end
+        end
+      end,
+    })
+  end,
   opts = {
     close_if_last_window = true,
     enable_git_status = true,
     enable_diagnostics = true,
+    open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
 
     filesystem = {
-      follow_current_file = {
-        enabled = true,
-      },
+      bind_to_cwd = false,
+      follow_current_file = { enabled = true },
       use_libuv_file_watcher = true,
     },
 
@@ -48,12 +68,26 @@ return {
         ["d"] = "delete",
         ["r"] = "rename",
         ["y"] = "copy_to_clipboard",
+        ["Y"] = {
+          function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            vim.fn.setreg("+", path, "c")
+          end,
+          desc = "Copy Path to Clipboard",
+        },
+        ["O"] = {
+          function(state)
+            require("lazy.util").open(state.tree:get_node().path, { system = true })
+          end,
+          desc = "Open with System Application",
+        },
         ["x"] = "cut_to_clipboard",
         ["p"] = "paste_from_clipboard",
         ["q"] = "close_window",
         ["<S-Tab>"] = "prev_source",
         ["<Tab>"] = "next_source",
-      }
+      },
     },
 
     sources = { "filesystem", "buffers", "git_status" },
