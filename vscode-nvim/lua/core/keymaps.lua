94a6466acff4ea9e5351313a2b1696bd4bda2fe6
NguyenXuanVinh <nongami87@gmail.com>
2025-10-14 00:10:35 +0700

    feat(nvim): add initial Neovim config for VSCode integration
    - introduces a modular Neovim configuration designed for use within VSCode.
    - sets up lazy plugin management, core options, keymaps, and autocommands.
    - adds VSCode-specific keybindings and which-key mappings to streamline navigation, editing, and integration with VSCode commands.
    - enhances user productivity by aligning Neovim's behavior and shortcuts with the VSCode workflow.

---
+++ vscode-nvim/lua/core/keymaps.lua
@@ -0,0 +1,54 @@
+local map = vim.keymap.set
+
+-- Better up/down
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true, desc = "Down" })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true, desc = "Up" })
+
+-- Window navigation (as remaps, per your request)
+map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+-- Move Lines
+map("n", "<A-j>", ":m .+1<CR>==", { desc = "Move Down" })
+map("n", "<A-k>", ":m .-2<CR>==", { desc = "Move Up" })
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move Down" })
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move Up" })
+
+-- Buffer/Tab navigation
+map("n", "<S-h>", function() vim.fn.VSCodeNotify("workbench.action.previousEditor") end, { desc = "Previous Editor" })
+map("n", "<S-l>", function() vim.fn.VSCodeNotify("workbench.action.nextEditor") end, { desc = "Next Editor" })
+map("n", "[b", function() vim.fn.VSCodeNotify("workbench.action.previousEditor") end, { desc = "Previous Editor" })
+map("n", "]b", function() vim.fn.VSCodeNotify("workbench.action.nextEditor") end, { desc = "Next Editor" })
+
+-- Clear search highlight on escape
+map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
+
+-- Clear search, diff update and redraw
+map(
+  "n",
+  "<leader>ur",
+  "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
+  { desc = "Redraw / Clear hlsearch / Diff Update" }
+)
+
+-- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+map("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next Search Result" })
+map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+map("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev Search Result" })
+map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+
+-- Better indenting
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+-- Commenting
+map("n", "gco", "o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add Comment Below" })
+map("n", "gcO", "O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add Comment Above" })
+
+-- Terminal
+map("t", "<esc>", "<C-\\><C-n>", { desc = "Enter Normal Mode" })
+map({ "n", "t" }, "<C-/>", function() vim.fn.VSCodeNotify("workbench.action.terminal.toggleTerminal") end, { desc = "Toggle Terminal" })
---
+++ vscode-nvim/lua/core/options.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = " "
+
+local opt = vim.opt
+
+opt.clipboard = "unnamedplus"
+opt.ignorecase = true
+opt.smartcase = true
