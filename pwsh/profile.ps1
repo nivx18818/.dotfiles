9f15608e9d6f1d8a08d56aacfb6b757ee3481b70
NguyenXuanVinh <nongami87@gmail.com>
2025-12-07 18:53:41 +0700

    feat(pwsh): enhance shell keybindings for navigation and completion
    - improves command-line usability by remapping Tab for menu completion and adding intuitive keybindings for word navigation and history search.
    - these changes streamline interactive workflows and make shell navigation more efficient.

--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -5,3 +5,10 @@ $Env:XDG_CONFIG_HOME="D:\code\dotfiles"
 $Env:NVIM_APPNAME="vscode-nvim"
 
 oh-my-posh init pwsh --config "D:\code\dotfiles\pwsh\poimandres.omp.json" | Invoke-Expression
+
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+Set-PSReadLineKeyHandler -Key "Ctrl+b" -Function BackwardWord
+Set-PSReadLineKeyHandler -Key "Ctrl+w" -Function ForwardWord
+Set-PSReadLineKeyHandler -Key 'Ctrl+p' -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key 'Ctrl+n' -Function HistorySearchForward
