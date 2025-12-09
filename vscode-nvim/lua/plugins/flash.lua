a54bc6569cb5417461664539b9e3be78bbfdde69
NguyenXuanVinh <nongami87@gmail.com>
2025-12-09 12:56:13 +0700

    feat(vscode-nvim): add `flash.nvim` plugin with custom highlighting and keymaps
    - integrates the `flash.nvim` plugin, configuring highlight groups for improved UI and setting up keybindings for jump, treesitter, and command-line toggling.
    - enhances navigation efficiency and visual clarity in the editor.

---
+++ vscode-nvim/lua/plugins/flash.lua
@@ -0,0 +1,20 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {},
+  config = function(_, opts)
+    require("flash").setup(opts);
+
+    vim.api.nvim_set_hl(0, "FlashBackdrop", { fg = "#767c9d" })
+    vim.api.nvim_set_hl(0, "FlashCurrent", { bg = "#ffffff", fg = "#1b1e28" })
+    vim.api.nvim_set_hl(0, "FlashLabel", { bg = "#5de4c7", fg = "#ffffff", bold = true })
+    vim.api.nvim_set_hl(0, "FlashMatch", { bg = "#272c3b", fg = "#ffffff" })
+    vim.api.nvim_set_hl(0, "FlashCursor", { reverse = true })
+  end,
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "<C-t>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+}
